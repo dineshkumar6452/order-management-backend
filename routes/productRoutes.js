2e6a65431168160aff1a0f3da69addd1da98b8ad
Dinesh <dineshk@adobe.com>
2025-05-21 23:57:58 +0530

    cache for get all products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const productController = require('../controllers/productController');
 const upload = require('../middleware/upload');
+const apicache = require('apicache');
+
+const cache = apicache.middleware;
 
 const router = express.Router();
 router.post("/products", upload.single("image"), productController.createProduct);
 router.put("/products/:id", upload.single("image"), productController.updateProduct);
 //router.post("/", productController.createProduct);
-router.get("/products", productController.getAllProducts);
+router.get("/products",cache('5 minutes'), productController.getAllProducts);
 router.get("/products/:id", productController.getProductById);
 //router.put("/:id", productController.updateProduct);
 router.delete("/products/:id", productController.deleteProduct);
