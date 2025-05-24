579e30eda4e32734264ca7e834da756b146036b0
Dinesh <dineshk@adobe.com>
2025-05-24 12:21:43 +0530

    supabase with URI

--- config/database.js
+++ config/database.js
@@ -8,18 +8,31 @@ require("dotenv").config();
 // });
 
 
-/// - super bas 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT, // should be 'postgres'
+/// - super bas  with IP 4
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   dialect: process.env.DB_DIALECT, // should be 'postgres'
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false, // required by Supabase
+//     },
+//     family: 4, // ✅ Force IPv4 to avoid ENETUNREACH in Render
+//   },
+//   logging: false, // Disable SQL logging
+// });
+
+// Supa base - URI
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // required by Supabase
+      rejectUnauthorized: false,
     },
-    family: 4, // ✅ Force IPv4 to avoid ENETUNREACH in Render
+    family: 4,
   },
-  logging: false, // Disable SQL logging
+  logging: false,
 });
 
 module.exports = sequelize
