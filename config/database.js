e63cac76e88da60524e5ceadc03d88bfc9e787d0
Dinesh <dineshk@adobe.com>
2025-05-24 12:05:27 +0530

    fixed for supabas ipv4

--- config/database.js
+++ config/database.js
@@ -8,9 +8,7 @@ require("dotenv").config();
 // });
 
 
-// module.exports = sequelize
-
-
+/// - super bas 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT, // should be 'postgres'
@@ -23,3 +21,5 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   },
   logging: false, // Disable SQL logging
 });
+
+module.exports = sequelize
